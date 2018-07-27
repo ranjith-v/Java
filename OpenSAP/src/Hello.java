b0ab0a678a6c576baed31725b340076affe4fb4c
Ranjith <Ranjith@Vallabhaneni-PC>
2018-07-28 04:35:11 +0530

    First commit
    Added all files

---
+++ OpenSAP/src/Hello.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int start = 2012;
+		int end = 2017;
+		double cases = 27;
+		System.out.println("Hello , I am Duke. From "+start+" to "+end+", I solved "+(cases / (end-start)) +" cases on avarage per year.");
+		System.out.println(Integer.parseInt("1337.54"));
+		
+		
+		Vechile v= new Vechile();
+		Vechile v2= new Vechile();
+		Vechile v3= new Vechile();
+		Vechile v4= new Vechile();
+		ArrayList<Vechile> myAL = new ArrayList<>();
+		//myAL.get("Duke");
+		//myAL.add("Duke");
+		
+		myAL.add(v);
+		myAL.get(0);
+
+	}
+
+}
---
+++ OpenSAP/src/Vechile.java
@@ -0,0 +1,12 @@
+
+public class Vechile {
+	public static int count;
+	public Vechile(){
+	 Vechile.count();
+	}
+	public static void count(){
+	  count = count+1; 
+	  System.out.println(count);
+	}
+
+}
